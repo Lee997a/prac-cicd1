22bc66c1afacf22f4ff1db4e2b9dfd062530bf52
Lee997a <ehddn5476@gmail.com>
2025-06-10 09:20:25 +0900

    cicd 1

---
+++ src/main/java/com/example/prac_cicd1/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.prac_cicd1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hi")
+    public String hi(){
+
+        return "hi cicd prac1";
+    }
+}
